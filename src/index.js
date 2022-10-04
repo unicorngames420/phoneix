1a7ce3229d7e61be18de1c4555f2a7a41618c02d
unicorngames420 <115004210+unicorngames420@users.noreply.github.com>
2022-10-04 15:00:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 100, 100);
 
-canvas.requestFullscreen();
+canvas.addEventListener("click", function() {
+  canvas.requestFullscreen();
+});
